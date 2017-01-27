e48f5a00f5450143002209feca8b6424fdde65d8
zcwist <zcwist@gmail.com>
2017-01-27 15:04:25 -0800

    master

---
+++ tutorial/myword2vec.py
@@ -0,0 +1,131 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import threading
+import time
+
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+import numpy as np
+import tensorflow as tf
+
+word2vec = tf.load_op_library(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'word2vec_ops.so'))
+
+flags = tf.app.flags
+
+flags.DEFINE_string("save_path", "tmp/", "Directory to write the model.")
+flags.DEFINE_string(
+    "train_data", "smalltext",
+    "Training data. E.g., unzipped file http://mattmahoney.net/dc/text8.zip.")
+flags.DEFINE_integer("embedding_size", 200, "The embedding dimension size.")
+flags.DEFINE_integer(
+    "epochs_to_train", 15,
+    "Number of epochs to train. Each epoch processes the training data once "
+    "completely.")
+flags.DEFINE_float("learning_rate", 0.025, "Initial learning rate.")
+flags.DEFINE_integer("num_neg_samples", 25,
+                     "Negative samples per training example.")
+flags.DEFINE_integer("batch_size", 500,
+                     "Numbers of training examples each step processes "
+                     "(no minibatching).")
+flags.DEFINE_integer("concurrent_steps", 12,
+                     "The number of concurrent training steps.")
+flags.DEFINE_integer("window_size", 5,
+                     "The number of words to predict to the left and right "
+                     "of the target word.")
+flags.DEFINE_integer("min_count", 5,
+                     "The minimum number of word occurrences for it to be "
+                     "included in the vocabulary.")
+flags.DEFINE_float("subsample", 1e-3,
+                   "Subsample threshold for word occurrence. Words that appear "
+                   "with higher frequency will be randomly down-sampled. Set "
+                   "to 0 to disable.")
+
+FLAGS = flags.FLAGS
+
+class Options(object):
+  """Options used by our word2vec model."""
+
+  def __init__(self):
+    # Model options.
+
+    # Embedding dimension.
+    self.emb_dim = FLAGS.embedding_size
+
+    # Training options.
+
+    # The training text file.
+    self.train_data = FLAGS.train_data
+
+    # Number of negative samples per example.
+    self.num_samples = FLAGS.num_neg_samples
+
+    # The initial learning rate.
+    self.learning_rate = FLAGS.learning_rate
+
+    # Number of epochs to train. After these many epochs, the learning
+    # rate decays linearly to zero and the training stops.
+    self.epochs_to_train = FLAGS.epochs_to_train
+
+    # Concurrent training steps.
+    self.concurrent_steps = FLAGS.concurrent_steps
+
+    # Number of examples for one training step.
+    self.batch_size = FLAGS.batch_size
+
+    # The number of words to predict to the left and right of the target word.
+    self.window_size = FLAGS.window_size
+
+    # The minimum number of word occurrences for it to be included in the
+    # vocabulary.
+    self.min_count = FLAGS.min_count
+
+    # Subsampling threshold for word occurrence.
+    self.subsample = FLAGS.subsample
+
+    # Where to write out summaries.
+    self.save_path = FLAGS.save_path
+    if not os.path.exists(self.save_path):
+      os.makedirs(self.save_path)
+
+class Word2Vec(object):
+    """Word2Vec model(Skipgram)"""
+    def __init__(self, options,session):
+        super(Word2Vec, self).__init__()
+        self._options = options
+        self._session = session
+        self._word2id = {}
+        self._id2word = []
+        self.build_graph()
+        self.save_vocab()
+
+    def build_graph(self):
+        """Build the model graph."""
+        opts = self._options
+        
+        # The training data. A text file.
+        (words, counts, words_per_epoch, current_epoch, total_words_processed, 
+        example, labels) = word2vec.skipgram_word2vec(filename=opts.train_data,
+                                                      batch_size=opts.batch_size，
+                                                      window_size=opts.window_size,
+                                                      min_count=opts.min_count,
+                                                      subsample=opts.subsample)
+
+        (opts.vocab_words, opts.vocab_counts,
+         opts.words_per_epoch) = self._session.run([words, counts, words_per_epoch])
+
+        print("Data file: ", opts.train_data)
+        print("Vocab size: ", opts.vocab_size - 1, " + UNK")
+        print("Words per epoch: ", opts.words_per_epoch)
+
+        
+
+
+
+    def save_vocab(self):
+        pass
+
+        
